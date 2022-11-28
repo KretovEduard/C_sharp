d64906f695a3e1f5bbd44530793652137cfbf24a
«Eduard» <edik_197431@mail.ru>
2022-11-28 08:10:53 +0300

    Решил Задачу 2

---
+++ 1_Helloconsole/Program.cs
@@ -0,0 +1,33 @@
+﻿//Console.WriteLine("Hello, World!");
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int a = 5,
+    b = 7,
+    max1;
+if (a > b)
+{max1 = a;}
+else
+{max1 = b;}
+Console.WriteLine(max1);
+
+int c = 2,
+    d = 10,
+    max2;
+if (c > d)
+{max2 = c;}
+else
+{max2 = d;}
+Console.WriteLine(max2);
+
+int e = -9,
+    f = -3,
+    max3;
+if (e > f)
+{max3 = e;}
+else
+{max3 = f;}
+Console.WriteLine(max3);

05e0b505cdd3f2ddba16f1a9edf6295e3d36b45d
«Eduard» <edik_197431@mail.ru>
2022-12-10 07:28:00 +0300

    Задача 29 решена

--- lesson4/27/Program.cs
+++ lesson4/27/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num % 10;
+if (num1 => 10)
+{
+    
+}
